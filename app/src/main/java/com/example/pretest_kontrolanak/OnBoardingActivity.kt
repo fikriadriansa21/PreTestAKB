cb856acb68b645a168016f01744edf150e74bbd6
fikriadriansa21 <fikriadriansa21@gmail.com>
2020-04-06 12:37:54 +0700

    pindahin komentar ke kelas utama

--- app/src/main/java/com/example/pretest_kontrolanak/OnBoardingActivity.kt
+++ app/src/main/java/com/example/pretest_kontrolanak/OnBoardingActivity.kt
@@ -4,15 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_oboarding.*
-
+//Nim : 10117128
+//Nama : Fikri Adriansa Yudha
+//commit : 5 April 2020, set intent dari halaman UserRegistrationActivity ke halaman ConfirmationInfoActivity, bikin layoutConfirmationInfoActivity, pass value edit text nama panggilan menggunakan putExtra
 class OnBoardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oboarding)
-        //Nim : 10117128
-        //Nama : Fikri Adriansa Yudha
-        //commit : 4 April 2020, set intent dari halaman KodeKeluargaActivity ke halaman UserRegistrationActivity, bikin layout User Registration, set Logic 6 character inputan dan all text Capslock default
+
         btn_mulai.setOnClickListener {
             // Handler code here.
             val intent = Intent(this, KodeKeluargaActivity::class.java)
